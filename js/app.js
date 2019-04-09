47ce1da5866d65cf53573a5758118d787910cbf9
DaysiGonzalez <dsgv11@hotmail.com>
2019-04-08 22:18:11 -0600

    feat: Added a second cat with it's click counter

--- js/app.js
+++ js/app.js
@@ -1,6 +1,20 @@
-let clicks = 0;
-var kittyImage = document.getElementById('kitty');
-kittyImage.addEventListener('click', function(){
-	clicks++;
-	document.getElementById("click-count").innerHTML = "Clicks: " + clicks;
+let firstCatClicks = 0;
+let secondCatClicks = 0;
+let firstCatName = 'George';
+let secondCatName = 'Willy';
+
+var firstCatImage = document.getElementById('first-cat-image');
+var secondCatImage = document.getElementById('second-cat-image');
+
+document.getElementById("first-cat-info").innerHTML = firstCatName + ': 0';
+document.getElementById("second-cat-info").innerHTML = secondCatName + ': 0';
+
+firstCatImage.addEventListener('click', function(){
+	firstCatClicks++;
+	document.getElementById("first-cat-info").innerHTML = firstCatName + ': ' + firstCatClicks;
+}, false);
+
+secondCatImage.addEventListener('click', function(){
+	secondCatClicks++;
+	document.getElementById("second-cat-info").innerHTML = secondCatName + ': ' + secondCatClicks;
 }, false);
