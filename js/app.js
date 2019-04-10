259197a9017ddb4c38330685403fbea6fde08119
DaysiGonzalez <dsgv11@hotmail.com>
2019-04-09 21:55:56 -0600

    style: Code sintax review

--- js/app.js
+++ js/app.js
@@ -43,19 +43,3 @@ catItem.addEventListener('click', (function(catName, catImg){
 }
 
 document.getElementById('cat-list').appendChild(catsList);
-
-// var firstCatImage = document.getElementById('first-cat-image');
-// var secondCatImage = document.getElementById('second-cat-image');
-//
-// document.getElementById("first-cat-info").innerHTML = firstCatName + ': 0';
-// document.getElementById("second-cat-info").innerHTML = secondCatName + ': 0';
-//
-// firstCatImage.addEventListener('click', function(){
-// 	firstCatClicks++;
-// 	document.getElementById("first-cat-info").innerHTML = firstCatName + ': ' + firstCatClicks;
-// }, false);
-//
-// secondCatImage.addEventListener('click', function(){
-// 	secondCatClicks++;
-// 	document.getElementById("second-cat-info").innerHTML = secondCatName + ': ' + secondCatClicks;
-// }, false);
